f89a6fcee4ed69094c720a4a12d84c8308729446
ZOUHAIR <zouhairmazi@example.com>
2026-04-13 19:43:46 +0100

    save local changes

--- resources/views/components/admin/navbar.blade.php
+++ resources/views/components/admin/navbar.blade.php
@@ -8,7 +8,15 @@
     class="fixed top-0 w-full z-50 bg-slate-50/80 dark:bg-slate-900/80 backdrop-blur-xl shadow-sm flex justify-between items-center px-8 h-16 w-full">
     <div class="flex items-center gap-8">
         <span class="text-xl font-extrabold tracking-tighter text-blue-950 dark:text-white">The Academic Curator</span>
-        
+        {{-- <div class="hidden md:flex items-center gap-6">
+            <a href="{{ route('admin.dashboard') }}"
+                class="{{ request()->routeIs('admin.dashboard') ? 'text-blue-900 dark:text-blue-100 font-bold border-b-2 border-blue-900 px-3 py-2 cursor-pointer active:scale-95 duration-200' : 'text-slate-500 dark:text-slate-400 font-medium hover:bg-slate-100 dark:hover:bg-slate-800 transition-colors px-3 py-2 rounded-lg cursor-pointer active:scale-95 duration-200' }}">Overview</a>
+            <a href="{{ route('admin.requests.index') }}"
+                class="{{ request()->routeIs('admin.requests.index') ? 'text-blue-900 dark:text-blue-100 font-bold border-b-2 border-blue-900 px-3 py-2 cursor-pointer active:scale-95 duration-200' : 'text-slate-500 dark:text-slate-400 font-medium hover:bg-slate-100 dark:hover:bg-slate-800 transition-colors px-3 py-2 rounded-lg cursor-pointer active:scale-95 duration-200' }}">All
+                Requests</a>
+            <a class="text-slate-500 dark:text-slate-400 font-medium hover:bg-slate-100 dark:hover:bg-slate-800 transition-colors px-3 py-2 rounded-lg cursor-pointer active:scale-95 duration-200"
+                href="#">Student Directory</a>
+        </div> --}}
     </div>
     <div class="flex items-center gap-4">
         <div class="relative group">
