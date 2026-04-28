3974c5c16d5bcf4010fcf1da650106e71f52aafa
ZOUHAIR <zouhairmazi@example.com>
2026-04-28 14:17:18 +0100

    Solving merge conflict ...

--- resources/views/Admin/students/index.blade.php
+++ resources/views/Admin/students/index.blade.php
@@ -128,10 +128,7 @@
                                 <td class="p-3 sm:p-5 text-xs sm:text-sm text-slate-700  md:table-cell whitespace-nowrap">{{ $student->cne ?? 'N/A' }}</td>
                                 <td class="p-3 sm:p-5 text-xs sm:text-sm text-slate-700  lg:table-cell whitespace-nowrap">{{ $student->department }}</td>
                                 <td class="p-3 sm:p-5">
-                                <td class="p-6 text-xs sm:text-sm text-slate-700 hidden sm:table-cell whitespace-nowrap">{{ $student->apogee_number }}</td>
-                                <td class="p-6 text-xs sm:text-sm text-slate-700 hidden md:table-cell whitespace-nowrap">{{ $student->cne ?? 'N/A' }}</td>
-                                <td class="p-6 text-xs sm:text-sm text-slate-700 hidden lg:table-cell whitespace-nowrap">{{ $student->department }}</td>
-                                <td class="p-6">
+
                                     @php
                                         $statusClasses = [
                                             'active' => ['bg-emerald-50', 'text-emerald-700'],
